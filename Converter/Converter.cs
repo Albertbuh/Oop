2e3c00693a59552fab4e54e1a370991f97f48a0b
albertbuh <albertbuhovets@mail.ru>
2023-04-11 21:36:29 +0300

    add converter with pdfCreation

---
+++ Converter/Converter.cs
@@ -0,0 +1,29 @@
+﻿namespace Converter;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+
+public class Converter 
+{
+  public static void ConvertToPdf(string header, string text, string path)
+  {
+    PdfWriter writer = new PdfWriter(path);
+    PdfDocument pdf = new PdfDocument(writer);
+    Document doc = new Document(pdf);
+    Paragraph pdf_header = new Paragraph(header).SetTextAlignment(TextAlignment.CENTER).SetFontSize(24);
+    doc.Add(pdf_header);
+    Paragraph p = new Paragraph(text).SetTextAlignment(TextAlignment.CENTER);
+    doc.Add(p);
+    doc.Close();
+  }
+  public static void ConvertToPdf(string text, string path)
+  {
+    PdfWriter writer = new PdfWriter(path);
+    PdfDocument pdf = new PdfDocument(writer);
+    Document doc = new Document(pdf);
+    Paragraph p = new Paragraph(text);
+    doc.Add(p);
+    doc.Close();
+  }
+}
