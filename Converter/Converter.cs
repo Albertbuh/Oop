a7235ce52648eccae7c00e1d678cbdaf2b847356
albertbuh <albertbuhovets@mail.ru>
2023-04-17 14:08:56 +0300

    add to converter json-xml convertation

--- Converter/Converter.cs
+++ Converter/Converter.cs
@@ -3,8 +3,12 @@ using iText.Kernel.Pdf;
 using iText.Layout;
 using iText.Layout.Element;
 using iText.Layout.Properties;
+using System.Xml.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
-public class Converter 
+public class Converter
 {
   public static void ConvertToPdf(string header, string text, string path)
   {
@@ -28,4 +32,32 @@ public class Converter
     doc.Add(p);
     doc.Close();
   }
+
+#nullable disable
+    private static readonly XDeclaration _defaultdeclaration = new XDeclaration("1.0", "utf-8", null);
+
+    public static void JsonToXml(string jsonFileName, string filename)
+    {
+      string js_text = File.ReadAllText(jsonFileName);
+      JArray ja = JArray.Parse(js_text); 
+
+      StringBuilder strXml = new StringBuilder(_defaultdeclaration.ToString() + "\n");
+      XDocument xml = new XDocument();
+
+      foreach(var item in ja)
+      {
+        StringBuilder strElement = new StringBuilder();
+        strElement.Append("{\n\"element\":");
+        strElement.AppendLine(item.ToString());
+        strElement.Append("}");
+        var elem = JsonConvert.DeserializeXmlNode(strElement.ToString());
+        xml = XDocument.Parse(elem.OuterXml);
+        strXml.AppendLine(xml.ToString());
+     }
+      Console.WriteLine(strXml.ToString());
+      if(filename.IndexOf(".xml") < 0)
+        filename = filename + ".xml";
+      File.WriteAllText(filename, strXml.ToString());
+    }
+#nullable restore
 }
